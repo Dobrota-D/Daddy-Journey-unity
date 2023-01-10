f38beffe365e1ffe772d02f918e736052a4d8e90
Brian <58400498+nairb725@users.noreply.github.com>
2023-01-10 19:05:10 +0100

    milk and map

---
+++ Assets/Scripts/itemCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class itemCollector : MonoBehaviour
+{
+    private int milks = 0;
+
+    [SerializeField] private Text milksText;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("milk"))
+        {
+            Destroy(collision.gameObject);
+            milks++;
+            milksText.text = "milks: " + milks;
+        }
+    }
+  
+}
