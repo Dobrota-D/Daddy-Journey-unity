87873e6cb80c550217de5cc6d68dd5c8ec9a36e3
Jado <jade.bouiges@edu.itescia.fr>
2023-01-12 00:57:54 +0100

    add count donw

---
+++ Assets/Scripts/CountDownController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownController : MonoBehaviour
+{
+    [SerializeField] int countDownTime;
+    [SerializeField] Text countDownDisplay;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject message;
+
+    private void Start()
+    {
+        player.SetActive(false);
+        message.SetActive(true);
+        StartCoroutine(CountDownToStart());
+        
+    }
+
+    IEnumerator CountDownToStart()
+    {
+  
+        //décompte 
+        countDownDisplay.text = "3";
+        yield return new WaitForSeconds(1f);
+        countDownDisplay.text = "2";
+        yield return new WaitForSeconds(1f);
+        countDownDisplay.text = "1";
+        yield return new WaitForSeconds(1f);
+        countDownDisplay.text = "GO !";
+
+        yield return new WaitForSeconds(1f);
+        //on cache le texte du décompte
+        countDownDisplay.gameObject.SetActive(false);
+        //on passe la variable isMoving à true pour pouvoir dans PlayerMouvement donner dees mouvements au Player
+        player.SetActive(true);
+        message.SetActive(false);
+
+    }
+}
